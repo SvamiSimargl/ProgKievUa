ccd56af2ba1d95997b00fc79927bf3a86581198b
SvamiSimargl <svamisimargl@mail.ru>
2017-09-10 16:05:40 +0300

    Add files via upload

--- Lesson1(Rectangle)/RectangleRunner.java
+++ Lesson1(Rectangle)/RectangleRunner.java
@@ -1,12 +1,11 @@
-package HW_1;
+package lesson1_rectangles;
 
 public class RectangleRunner {
     public static void main(String[] args) {
-
         Rectangles rectangles = new Rectangles();
-        rectangles.addNewRectangle(1.5, 2.8);
-        rectangles.addNewRectangle(1.2, 2.3);
-        rectangles.addNewRectangle(6.5, 3.8);
+        rectangles.addNewRectangle(1.9, 2.3);
+        rectangles.addNewRectangle(3.9, 1.8);
+        rectangles.addNewRectangle(1.4, 2.0);
         System.out.println(rectangles.getTotalSumm());
 
     }
