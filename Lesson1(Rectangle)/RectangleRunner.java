1e905eaa9f1b8ee85de61f867e90939af94de867
SvamiSimargl <svamisimargl@mail.ru>
2017-09-10 15:57:08 +0300

    Update RectangleRunner.java

--- Lesson1(Rectangle)/RectangleRunner.java
+++ Lesson1(Rectangle)/RectangleRunner.java
@@ -4,7 +4,9 @@ public class RectangleRunner {
     public static void main(String[] args) {
 
         Rectangles rectangles = new Rectangles();
-        rectangles.addNewRectangle(1, 2);
+        rectangles.addNewRectangle(1.5, 2.8);
+        rectangles.addNewRectangle(1.2, 2.3);
+        rectangles.addNewRectangle(6.5, 3.8);
         System.out.println(rectangles.getTotalSumm());
 
     }
