f7e754ca6ee86b06080413a476766ee3b20e7e32
SvamiSimargl <svamisimargl@mail.ru>
2017-09-10 15:54:57 +0300

    Add files via upload

---
+++ Lesson1(Rectangle)/RectangleRunner.java
@@ -0,0 +1,12 @@
+package HW_1;
+
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        Rectangles rectangles = new Rectangles();
+        rectangles.addNewRectangle(1, 2);
+        System.out.println(rectangles.getTotalSumm());
+
+    }
+
+}
---
+++ Lesson1(Rectangle)/Rectangles.java
@@ -0,0 +1,32 @@
+package HW_1;
+
+import java.util.ArrayList;
+
+public class Rectangles {
+
+
+    private ArrayList<Rectangle> rectangles = new ArrayList<>();
+
+
+    public ArrayList<Rectangle> getRectangles() {
+        return rectangles;
+    }
+
+
+    public void addNewRectangle(double height, double width) {
+        rectangles.add(new Rectangle(height, width));
+
+    }
+
+    public double getTotalSumm() {
+        double totalSumm = 0;
+        for (int i = 0; i < rectangles.size(); i++) {
+            totalSumm += rectangles.get(i).Area();
+
+        }
+        return totalSumm;
+
+    }
+
+
+}
